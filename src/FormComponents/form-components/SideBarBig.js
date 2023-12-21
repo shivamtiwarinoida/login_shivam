eef975a0bacb08cb46f60c1a21d60d541501c935
Shivam Tiwari <113277441+shivamtiwarinoida@users.noreply.github.com>
2023-12-21 12:20:15 +0530

    commit

--- src/FormComponents/form-components/SideBarBig.js
+++ src/FormComponents/form-components/SideBarBig.js
@@ -18,14 +18,14 @@ const SideBarBig = ({ val, setVal }) => {
     return (
         <ul className={val ? 'sidebar-big' : 'hidden'}>
             <li className='not-apply-big'><span></span><img src={cross} onClick={handle}/></li>
-            <li  className='sidebar-big-li'><img src={home}  /><Link to="/" className='side-link'>HOME</Link></li>
-            <li className='sidebar-big-li'><img src={event} /><Link to="/event" className='side-link'>EVENT</Link></li>
-            <li className='sidebar-big-li'><img src={team}  /><Link to="/" className='side-link'>TEAM</Link></li>
-            <li className='sidebar-big-li'><img src={brochure}  /><Link to="/" className='side-link'>BROCHURE</Link></li>
-            <li className='sidebar-big-li'><img src={sponser}  /><Link to="/" className='side-link'>SPONSOR</Link></li>
-            <li className='sidebar-big-li'><img src={tick}  /><Link to="/signin" className='side-link'>Register</Link></li>
+            <li  className='sidebar-big-li'><Link to="/" className='side-link'><img src={home}  />HOME</Link></li>
+            <li className='sidebar-big-li'><Link to="/event" className='side-link'><img src={event} />EVENT</Link></li>
+            <li className='sidebar-big-li'><Link to="/" className='side-link'><img src={team}  />TEAM</Link></li>
+            <li className='sidebar-big-li'><Link to="/" className='side-link'><img src={brochure}  />BROCHURE</Link></li>
+            <li className='sidebar-big-li'><Link to="/" className='side-link'><img src={sponser}  />SPONSOR</Link></li>
+            <li className='sidebar-big-li'><Link to="/signin" className='side-link'><img src={tick}  />Register</Link></li>
             <li className='not-apply'></li>
-            <li className='sidebar-big-li'><img src={login}  /><Link to="/login" className='side-link'>Login</Link></li>
+            <li className='sidebar-big-li'><Link to="/login" className='side-link'><img src={login}  />Login</Link></li>
         </ul>
     )
 }
